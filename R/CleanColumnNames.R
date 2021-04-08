a0540bb538335a3c3e5552fb1be02e705c7c236f
lgorman <lgorman@turing.ac.uk>
2021-04-08 10:21:07 +0100

    modifying the loop names in clean column names

--- R/CleanColumnNames.R
+++ R/CleanColumnNames.R
@@ -45,30 +45,88 @@ shorten_multiple_column_names <- function(long_names, seperator){
     split_list <- unlist(lapply(long_names,function(name) shorten_individual_column_name(name,seperator)))
 }
 #---------------------------------------------------------------------------------------------------
+#' Modify Loop Name
+#'
+#' @param column_name
+#' @param loop_type
+#' @param seperator
+#'
+#' @return
+#' @export
+#'
+#' @examples
+modify_loop_name <- function(column_name, loop_type){
 
-#' Title
+    # Get rid of everything before the opening square bracket
+    # Can modify and generalise to exclude the "loop_type" variable
+    pattern <- paste0(".*",loop_type,"\\[")
+    repeat_number <- gsub(pattern,"",column_name)
+    # Get rid of everything after closing square bracket
+    repeat_number <- gsub("\\].*","",repeat_number)
+
+    # Combining original column name with extra number
+    new_column_name <- paste0(column_name,"_",repeat_number)
+    return(new_column_name)
+}
+
+
+
+#---------------------------------------------------------------------------------------------------
+
+#' Modify Loop Names
 #'
 #' @param column_names The column names which need to be shortened
-#' @param type What type of loop is concerned, the options include "crop_repeat", "livestock_repeat", "offfarm_income_repeat".
+#' @param loop_type What loop_type of loop is concerned, the options include "crop_repeat", "livestock_repeat", "offfarm_income_repeat".
 #' @param seperator The string separating parts of each column name
 #'
 #' @return A list of column names which need to be modified
 #' @export
 #'
 #' @examples
-modify_loop_column_names <- function(column_names, type, seperator) {
-    type <- "crop_repeat"
-    column_names <- c("SECTION_Crop_Productivity/crop_repeat[1]/crop_name",
-     "SECTION_Crop_Productivity/crop_repeat[2]/crop_name",
-     "SECTION_Crop_Productivity/crop_repeat[3]/crop_name",
-     "SECTION_Crop_Productivity/crop_repeat[4]/crop_name",
-     "SECTION_Crop_Productivity/crop_repeat[5]/crop_name",
-     "SECTION_Crop_Productivity/crop_repeat/crop_name")
+modify_loop_column_names <- function(column_names, loop_type) {
+    # Finding all of the items with pattern "xxx/type[number]/column_name"
+   repeat_columns <- grep(paste0(loop_type,"\\[.\\]"),column_names)
 
-   repeat_columns <- grep(paste0(type,".[:alnum:]."),column_names)
+   # Modifying the relevant columns
+   column_names[repeat_columns] <- unlist(lapply(column_names[repeat_columns], function(x) modify_loop_name(x, loop_type)))
+    return(column_names)
 
 }
 #---------------------------------------------------------------------------------------------------
+#' Modify all types of loop in RHoMIS
+#'
+#' The RHoMIS survey is structured using a series of important loops. These are often labelled "crop_repeat[x]".
+#' It is important to be modify all of these loops to have a simpler column name format for later analysis
+#'
+#' @param column_names A list of all of the column names which
+#' @param repeat_columns A list of all of the types of repeat column which need to be changed
+#'
+#' @return
+#' @export
+#'
+#' @examples
+modify_all_loop_column_names <- function(column_names, repeat_columns){
+    for (repeat_column in repeat_columns){
+        column_names<-modify_loop_column_names(column_names, repeat_column)
+    }
+    return(column_names)
+}
+#---------------------------------------------------------------------------------------------------
 
+#' Clean Column Names
+#'
+#' A single functions to shorten and clean all RHoMIS column names.
+#'
+#' @param column_names
+#' @param seperator
+#' @param repeat_columns
+#'
+#' @return
+#' @export
+#'
+#' @examples
+clean_column_names <- function(column_names, seperator, repeat_columns){
+
+}
 
 
--- tests/testthat/test-CleanColumnNames.R
+++ tests/testthat/test-CleanColumnNames.R
@@ -1,5 +1,6 @@
 library(testthat)
 
+# Test for shorten_individual_column_name
 test_that("Raw column names successfully shorten", {
     long_name <- "xxxx/xxxxx/xxxxx/important_name"
     seperator <- "/"
@@ -7,12 +8,65 @@ test_that("Raw column names successfully shorten", {
     expect_equal(shortened_name, "important_name")
 })
 
+# Test for shorten_multiple_column_names
 test_that("Multiple column names switched properly",{
     long_names <- c("xxxx/xxxxx/xxxxx/important_name_1",
                     "xxxx/xxxxx/xxxxx/important_name_2",
                     "xxxx/xxxxx/xxxxx/important_name_3")
     seperator <- "/"
-    split_list <- unlist(lapply(long_names,function(name) shorten_individual_column_name(name,seperator)))
-    result_equals_expectation <- all(split_list==c("important_name_1","important_name_2","important_name_3"))
-    expect_equal(result_equals_expectation,TRUE)
+    actual_result <- shorten_multiple_column_names(long_names, seperator)
+    expected_result <- c("important_name_1","important_name_2","important_name_3")
+    expect_equal(actual_result,expected_result)
+})
+
+# Test for modify_loop_name
+test_that("Repeat loop single can be changed",{
+    column_name <- "SECTION_Crop_Productivity/crop_repeat[2]/crop_name"
+    loop_type <- "crop_repeat"
+    modified_name <- modify_loop_name(column_name, loop_type)
+
+    expect_equal(modified_name, "SECTION_Crop_Productivity/crop_repeat[2]/crop_name_2")
+})
+
+# Test for modifying multiple column names
+test_that("Multiple repeat loops changed correctly", {
+    loop_type <- "crop_repeat"
+    original_names <- c("SECTION_Crop_Productivity/crop_repeat[1]/crop_name",
+                      "SECTION_Crop_Productivity/crop_repeat[2]/crop_name",
+                      "SECTION_Crop_Productivity/crop_repeat[3]/crop_name",
+                      "SECTION_Crop_Productivity/crop_repeat/crop_name",
+                      "SECTION_Crop_Productivity/crop_repeat[4]/crop_name",
+                      "SECTION_Crop_Productivity/crop_repeat[5]/crop_name",
+                      "SECTION_Crop_Productivity/crop_repeat/crop_name")
+    expected_result <- c(result <- c("SECTION_Crop_Productivity/crop_repeat[1]/crop_name_1",
+                                     "SECTION_Crop_Productivity/crop_repeat[2]/crop_name_2",
+                                     "SECTION_Crop_Productivity/crop_repeat[3]/crop_name_3",
+                                     "SECTION_Crop_Productivity/crop_repeat/crop_name",
+                                     "SECTION_Crop_Productivity/crop_repeat[4]/crop_name_4",
+                                     "SECTION_Crop_Productivity/crop_repeat[5]/crop_name_5",
+                                     "SECTION_Crop_Productivity/crop_repeat/crop_name"))
+    actual_result <- modify_loop_column_names(original_names,loop_type)
+    expect_equal(expected_result,actual_result)
+})
+
+# Testing that all types of repeat loop can be modified
+test_that("All types of repeat loop can be specified and modified",{
+    repeat_columns<- c("crop_repeat", "livestock_repeat", "offfarm_repeat", "hh_rep")
+    column_names <- c("xxx/crop_repeat[1]/crop_name",
+                      "xxx/livestock_repeat[2]/livestock_name",
+                      "xxx/crop_repeat[3]/crop_name",
+                      "xx/crop_repeat/crop_name",
+                      "x/offfarm_repeat[4]/offfarm_name",
+                      "y/hh_rep[5]/person_name",
+                      "z/crop_repeat/crop_name")
+    expected_result <- c("xxx/crop_repeat[1]/crop_name_1",
+                         "xxx/livestock_repeat[2]/livestock_name_2",
+                         "xxx/crop_repeat[3]/crop_name_3",
+                         "xx/crop_repeat/crop_name",
+                         "x/offfarm_repeat[4]/offfarm_name_4",
+                         "y/hh_rep[5]/person_name_5",
+                         "z/crop_repeat/crop_name")
+    actual_result <- modify_all_loop_column_names(column_names,repeat_columns)
+
+    expect_equal(expected_result,actual_result)
 })
