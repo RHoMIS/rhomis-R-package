4aaa93365336ac65a542c0d88f8c81a55ce5dbbd
lgorman <lgorman@turing.ac.uk>
2021-04-26 15:52:57 +0100

    adding github action for R cran check

--- R/CleanColumnNames.R
+++ R/CleanColumnNames.R
@@ -1,3 +1,5 @@
+#' This script includes a series of functions which can be used to help clean
+#' column names of data collected with RHoMIS.
 
 #---------------------------------------------------------------------------------------------------
 #' Shorten Individual Name
@@ -25,10 +27,14 @@ shorten_individual_column_name <- function(column_name, seperator){
 #---------------------------------------------------------------------------------------------------
 #' Shorten Multiple Column Names
 #'
+#' Used to abbreviate multiple column names based on their last values. Typical RHoMIS column names come
+#' in the for "xxxx/xxxxx/xxxxx/important_name". This function helps to extract only the final value
+#' "important name". It will do this for multiple column names
+#'
 #' @param column_names The list of column names which need to be shortened
 #' @param seperator  The string seperating parts of each column name
 #'
-#' @return A list of
+#' @return A list of shortened column names
 #' @export
 #'
 #' @examples
@@ -38,7 +44,8 @@ shorten_individual_column_name <- function(column_name, seperator){
 #'                 "xxxx/xxxxx/xxxxx/important_name_3")
 #' seperator <- "/"
 #'
-#' split_list <- unlist(lapply(long_names,function(name) shorten_individual_column_name(name,seperator)))
+#' shortened_names <- shorten_multiple_column_names(long_names, seperator)
+#' # shortened_names will equal c("important_name_1","important_name_2", "important_name_3")
 
 shorten_multiple_column_names <- function(long_names, seperator){
 
@@ -47,9 +54,10 @@ shorten_multiple_column_names <- function(long_names, seperator){
 #---------------------------------------------------------------------------------------------------
 #' Modify Loop Name
 #'
+#' Many of the variables in RHoMIS are collected through a looping structure
+#'
 #' @param column_name
 #' @param loop_type
-#' @param seperator
 #'
 #' @return
 #' @export
@@ -75,14 +83,28 @@ modify_loop_name <- function(column_name, loop_type){
 
 #' Modify Loop Names
 #'
-#' @param column_names The column names which need to be shortened
-#' @param loop_type What loop_type of loop is concerned, the options include "crop_repeat", "livestock_repeat", "offfarm_income_repeat".
-#' @param seperator The string separating parts of each column name
+#' A function to modify column names for RHoMIS loops
+#'
+#' The RHoMIS survey is structured using a series of important loops. These are often labelled "xxxx/crop_repeat[1]/crop_name", "xxxx/crop_repeat[2]/crop_name", "xxxx/crop_repeat[3]/crop_name"...
+#'
+#' This function aims to modify those into a more user-friendly format "crop_name_1", "crop_name_2",
+#' "crop_name_3" ...
+#'
+#' @param column_names A list of the column names which need to be shortened. This would generally be all of the column names of the RHoMIS dataset.
+#' @param loop_type What loop_type of loop is concerned, this could include options such as "crop_repeat", "livestock_repeat", "offfarm_income_repeat". It is very important that this "type" matches the pattern in the original column names
 #'
 #' @return A list of column names which need to be modified
 #' @export
 #'
 #' @examples
+#' original_columns <- c("x/non_repeat_column","xxxx/crop_repeat[1]/crop_name", "xxxx/crop_repeat[2]/crop_name")
+#' loop_type <- "crop_repeat" # See here how the look type matches exactly the pattern in the "original_columns"
+#'
+#' modified_names <- modify_loop_column_names(original_columns, loop_type)
+#' # modified_names will equal "x/non_repeat_column","xxxx/crop_repeat[1]/crop_name_1", "xxxx/crop_repeat[2]/crop_name_2"
+#'
+#' # You can then use the function "shorten_multiple_column_names" to further tidy the column names
+#'
 modify_loop_column_names <- function(column_names, loop_type) {
     # Finding all of the items with pattern "xxx/type[number]/column_name"
     repeat_columns <- grep(paste0(loop_type,"\\[.\\]"),column_names)
@@ -97,6 +119,7 @@ modify_loop_column_names <- function(column_names, loop_type) {
 #'
 #' The RHoMIS survey is structured using a series of important loops. These are often labelled "crop_repeat[x]".
 #' It is important to be modify all of these loops to have a simpler column name format for later analysis
+#'
 #'
 #' @param column_names A list of all of the column names which
 #' @param repeat_columns A list of all of the types of repeat column which need to be changed
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # RHoMIS R-package for data processing
 
+<!-- badges: start -->
+[![R-CMD-check](https://github.com/l-gorman/rhomis-R-package/workflows/R-CMD-check/badge.svg)](https://github.com/l-gorman/rhomis-R-package/actions)
+<!-- badges: end -->
+
+
 This package enables complete processing of RHoMIS data. This processing occurs in X main steps.
 Data is downloaded from the RHoMIS ODK central server. Data is cleaned, column names are simplified, new units are 
 extracted. Data is reformatted, data collected in an ODK loop structure is formatted for analysis. Reformatted data
