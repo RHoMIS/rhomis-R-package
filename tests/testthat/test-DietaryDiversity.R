6318acc8e96b290ddc16226a6b58e5004b4d625d
lgorman <lgorman@turing.ac.uk>
2021-05-20 19:50:01 +0100

    adding data files and HDDS calculation scripts

--- tests/testthat/test-DietaryDiversity.R
+++ tests/testthat/test-DietaryDiversity.R
@@ -1,3 +1,32 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(testthat)
+
+test_that("HDDS Calculations work", {
+
+
+    actual_result <-hdds_calc(hdds_10_test)
+    expected_result <- structure(list(HDDS_good_season = c(8, 10, 10, 9, 8),
+                                      HDDS_good_season_bought = c(2, 5, 4, 5, 3),
+                                      HDDS_good_season_farm = c(6, 7, 6, 4, 5),
+                                      HDDS_bad_season = c(4, 8, 6, 6, 5),
+                                      HDDS_bad_season_bought = c(4, 5, 6, 6, 5),
+                                      HDDS_bad_season_farm = c(0, 3, 0, 0, 0),
+                                      HDDS_last_month = c(0, 0, 0, 0, 0)),
+                                 row.names = c(NA, -5L), class = c("tbl_df", "tbl", "data.frame"))
+
+
+  expect_equal(actual_result, expected_result)
+
+  actual_result <-hdds_calc(hdds_14_test)
+  expected_result <- structure(list(HDDS_bad_season = c(0, 4, 0, 5, 5),
+                                    HDDS_bad_season_bought = c(0, 4, 0, 3, 4),
+                                    HDDS_bad_season_farm = c(0, 0, 0, 4, 4),
+                                    HDDS_good_season = c(0, 5, 0, 4, 8),
+                                    HDDS_good_season_bought = c(0, 5, 0, 2, 7),
+                                    HDDS_good_season_farm = c(0, 0, 0, 3, 5),
+                                    HDDS_last_month = c(9, 6, 5, 4, 7),
+                                    HDDS_last_month_bought = c(6, 0, 3, 0, 0),
+                                    HDDS_last_month_farm = c(5, 0, 5, 0, 0)),
+                               row.names = c(NA, -5L), class = c("tbl_df", "tbl", "data.frame"))
+
+  expect_equal(actual_result, expected_result)
 })
--- tests/testthat/test-ProgressOutOfPoverty.R
+++ tests/testthat/test-ProgressOutOfPoverty.R
@@ -1,3 +1,23 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("PPI calculation works", {
+    data <- as_tibble(list("PPI_1"=c(5,1,3,4),
+                           "PPI_2"=c(3,12,7,18),
+                           "PPI_3"=c(17,1,2,3),
+                           "PPI_4"=c(NA,2,1,6),
+                           "PPI_5"=c(1,NA,3,1),
+                           "PPI_6"=c(3,7,4,1),
+                           "PPI_7"=c(8,3,NA,2),
+                           "PPI_8"=c(3,8,1,3),
+                           "PPI_9"=c(9,2,8,5),
+                           "PPI_10"=c(12,4,7,1),
+                           "random_other_column"=c(NA,NA,NA,NA)))
+
+    country_code_column <- c("VN","VN","KE","KE")
+
+    expected_result <- structure(list(ppi_likelihood = c(14.1, 52, 38.8, 26.5),
+                                      ppi_limit = c(1.75, 1.75, 1.9, 1.9)),
+                                 row.names = c(NA, -4L), class = c("tbl_df", "tbl", "data.frame"))
+
+    actual_result <- ppi_score(data,country_code_column)
+
+  expect_equal(actual_result, expected_result)
 })
