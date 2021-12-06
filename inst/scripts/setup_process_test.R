86bbcb622a2e97894bc00821dd07c6f2af845a9c
l-gorman <lgorman@turing.ac.uk>
2021-12-06 16:31:54 +0000

    fixing so tests past hopefully

--- inst/scripts/setup_process_test.R
+++ inst/scripts/setup_process_test.R
@@ -3,46 +3,46 @@ readRenviron(".env")
 
 
 
-# dataSource="csv"
-# outputType="csv"
-# coreOnly=T
-# surveyFile=NULL
-# moduleSaving=F
+dataSource="csv"
+outputType="csv"
+coreOnly=T
+surveyFile=NULL
+moduleSaving=F
+extractUnits=F
+processDataSet=T
+dataFilePath="inst/extdata/projects/KE_ESA_2021/Raw_Data.csv"
+central_url=NULL
+central_email=NULL
+central_password=NULL
+project_name=NULL
+form_name=NULL
+form_version=NULL
+database=NULL
+draft=NULL
+
+
+# dataSource="central"
+# outputType="mongodb"
+# coreOnly=F
+# moduleSaving=T
 # extractUnits=F
 # processDataSet=T
 # dataFilePath=NULL
 # central_url=paste0("https://",Sys.getenv("CENTRALURL"))
 # central_email=Sys.getenv("CENTRALEMAIL")
 # central_password=Sys.getenv("CENTRALPASSWORD")
-# project_name="wsedfghfdxxdcvb"
+# project_name="u1p1"
 # form_name="form1"
 # form_version="version_xyz"
 # database="rhomis-data-dev"
 # draft=TRUE
-
-
-dataSource="central"
-outputType="mongodb"
-coreOnly=F
-moduleSaving=T
-extractUnits=F
-processDataSet=T
-dataFilePath=NULL
-central_url=paste0("https://",Sys.getenv("CENTRALURL"))
-central_email=Sys.getenv("CENTRALEMAIL")
-central_password=Sys.getenv("CENTRALPASSWORD")
-project_name="u1p1"
-form_name="form1"
-form_version="version_xyz"
-database="rhomis-data-dev"
-draft=TRUE
-
+#
 
 processData(
-    dataSource="central",
+    dataSource="csv",
     outputType="mongodb",
-    coreOnly=F,
-    moduleSaving=T,
+    coreOnly=T,
+    moduleSaving=F,
     extractUnits=T,
     processDataSet=F,
     dataFilePath=NULL,
