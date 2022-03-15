fad01c86d096956854aca01f2daa17af69651ecd
l-gorman <lgorman@turing.ac.uk>
2022-03-15 10:57:06 +0000

    adding skeleton for indicator mapping object

---
+++ data-raw/indicator-mapping.R
@@ -0,0 +1,31 @@
+#' A mapping of all of the indicators calculated in RHoMIS.
+#'
+#' This comes in the form of a list, each item in the list
+#' looks like this
+#'
+#' {
+#' indicator_name:  "crop_yield", # "the name of the indicator being calculated",
+#' individual_columns_required: [], #
+#' loop_columns_required: [], #
+#' indicators_required: [], #
+#' file_located: "" #,
+#'
+#'
+#'
+#' }
+#'
+
+
+json_mapping <- '
+[
+    {
+
+    },
+
+
+    {
+
+    }
+
+]
+'
