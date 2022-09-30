7c716c86e4372c8a5beae6ab902e303aea2dd44a
l-gorman <lgorman@turing.ac.uk>
2022-09-30 12:44:36 +0100

    updating server tests to make sure they don't run on windows

--- tests/testthat/test-02-calculate-prices.R
+++ tests/testthat/test-02-calculate-prices.R
@@ -75,7 +75,7 @@ test_that("Can run second calculation stage locally", {
         suppressWarnings(calculate_prices_csv(
             base_path="./",
             file_path,
-             id_type=id_type,
+            id_type=id_type,
             proj_id,
             form_id
         ))
@@ -118,80 +118,83 @@ test_that("Can run second calculation stage locally", {
 
 test_that("Can run second calculation stage on server", {
 
-    result = tryCatch({
+    if(.Platform$OS.type!="windows")
+    {
+        result = tryCatch({
 
-        central_url <- "https://github.com/l-gorman/rhomis-R-package/blob/main/inst/sample_central_project/sample-central-data.csv.zip?raw=true"
-        central_email <- "test@domain.com"
-        central_password <- "testpassword"
-        project_name <- "test_project"
-        form_name <- "test_form"
-        database <- "rhomis-test"
-        isDraft <- F
-        central_test_case <- T
+            central_url <- "https://github.com/l-gorman/rhomis-R-package/blob/main/inst/sample_central_project/sample-central-data.csv.zip?raw=true"
+            central_email <- "test@domain.com"
+            central_password <- "testpassword"
+            project_name <- "test_project"
+            form_name <- "test_form"
+            database <- "rhomis-test"
+            isDraft <- F
+            central_test_case <- T
 
-        suppressWarnings(extract_units_and_conversions_server(
-            central_url = central_url,
-            central_email = central_email,
-            central_password = central_password,
-            project_name = project_name,
-            form_name = form_name,
-            database = database,
-            isDraft = isDraft,
-            central_test_case = central_test_case
-        ))
-        suppressWarnings(calculate_prices_server(
-            central_url = central_url,
-            central_email = central_email,
-            central_password = central_password,
-            project_name = project_name,
-            form_name = form_name,
-            database = database,
-            isDraft = isDraft,
-            central_test_case = central_test_case
-        ))
+            suppressWarnings(extract_units_and_conversions_server(
+                central_url = central_url,
+                central_email = central_email,
+                central_password = central_password,
+                project_name = project_name,
+                form_name = form_name,
+                database = database,
+                isDraft = isDraft,
+                central_test_case = central_test_case
+            ))
+            suppressWarnings(calculate_prices_server(
+                central_url = central_url,
+                central_email = central_email,
+                central_password = central_password,
+                project_name = project_name,
+                form_name = form_name,
+                database = database,
+                isDraft = isDraft,
+                central_test_case = central_test_case
+            ))
 
 
-        TRUE
+            TRUE
 
-    },
-    error=function(error){
-        traceback()
-        print(error)
-        return(FALSE)
+        },
+        error=function(error){
+            traceback()
+            print(error)
+            return(FALSE)
 
-    },
+        },
 
-    finally={
-        # Remove test entries from database
-        data_collection <- mongolite::mongo(collection = "data",
-                                            db = "rhomis-test",
-                                            url =  "mongodb://localhost")
-        data_collection$drop()
-        data_collection$disconnect()
+        finally={
+            # Remove test entries from database
+            data_collection <- mongolite::mongo(collection = "data",
+                                                db = "rhomis-test",
+                                                url =  "mongodb://localhost")
+            data_collection$drop()
+            data_collection$disconnect()
 
-        project_data_collection <- mongolite::mongo(collection = "projectData",
-                                                    db = "rhomis-test",
-                                                    url =  "mongodb://localhost")
-        project_data_collection$drop()
-        project_data_collection$disconnect()
+            project_data_collection <- mongolite::mongo(collection = "projectData",
+                                                        db = "rhomis-test",
+                                                        url =  "mongodb://localhost")
+            project_data_collection$drop()
+            project_data_collection$disconnect()
 
-        units_and_conversion_collection <- mongolite::mongo(collection = "units_and_conversions",
-                                                            db = "rhomis-test",
-                                                            url =  "mongodb://localhost")
-        units_and_conversion_collection$drop()
-        units_and_conversion_collection$disconnect()
+            units_and_conversion_collection <- mongolite::mongo(collection = "units_and_conversions",
+                                                                db = "rhomis-test",
+                                                                url =  "mongodb://localhost")
+            units_and_conversion_collection$drop()
+            units_and_conversion_collection$disconnect()
 
-        units_and_conversion_collection <- mongolite::mongo(collection = "unmodified_units",
-                                                            db = "rhomis-test",
-                                                            url =  "mongodb://localhost")
-        units_and_conversion_collection$drop()
-        units_and_conversion_collection$disconnect()
+            units_and_conversion_collection <- mongolite::mongo(collection = "unmodified_units",
+                                                                db = "rhomis-test",
+                                                                url =  "mongodb://localhost")
+            units_and_conversion_collection$drop()
+            units_and_conversion_collection$disconnect()
 
-        print("Database cleared")
+            print("Database cleared")
 
-    })
+        })
 
-    expect_equal(result, TRUE)
+        expect_equal(result, TRUE)
+    }
 })
 
 
